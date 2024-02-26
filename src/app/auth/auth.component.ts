eb1c1f36fe95669b936563bba14c410d0d608df9
Ashiq807 <ashiqos807@gmail.com>
2024-02-26 18:49:24 +0530

    9th commit

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,8 @@
 import { Component } from "@angular/core"; 
 import { NgForm } from "@angular/forms"; 
 
-import { AuthService } from "./auth.service";
+import { AuthResponseData, AuthService } from "./auth.service";
+import { Observable } from "rxjs";
 
 @Component({
     selector: 'app-auth',
@@ -25,22 +26,26 @@ export class AuthComponent {
         const email = form.value.email;
         const password = form.value.password;
 
+        let authObs: Observable<AuthResponseData>;
+
         this.isLoading = true;
         if(this.isLoginMode) {
-
+           authObs = this.authService.login(email, password);
         } else {
-            this.authService.signup(email, password).subscribe(
-                {
-                    next: resData=>{console.log(resData);
-                    this.isLoading = false;
-                    },
-                    error: error=>{console.log(error);
-                    this.error = "An error occured";
-                    this.isLoading = false;
-                    }
-                }
-            );    
+           authObs = this.authService.signup(email, password);  
         }
+
+        authObs.subscribe(
+            {
+                next: resData=>{console.log(resData);
+                this.isLoading = false;
+                },
+                error: errorMessage=>{console.log(errorMessage);
+                    this.error = errorMessage;
+                this.isLoading = false;
+                }
+            }
+        ); 
 
         form.reset();
     }
